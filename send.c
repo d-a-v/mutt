45cb791891f14a40c5529a8d5333c06c9d10deb4
Job Snijders <job@instituut.net>
2017-06-29 13:08:09 +0200

    derp

--- send.c
+++ send.c
@@ -1337,14 +1337,14 @@ ci_send_message (int flags,		/* send mode */
         mutt_set_parameter ("format", "flowed", &msg->content->parameter);
     }
 
+    if (option (OPTUSEFROMASBCC))
+          msg->env->bcc = mutt_default_from ();
     /* $use_from and/or $from might have changed in a send-hook */
     if (killfrom)
     {
       rfc822_free_address (&msg->env->from);
       if (option (OPTUSEFROM) && !(flags & (SENDPOSTPONED|SENDRESEND))) {
         msg->env->from = mutt_default_from ();
-        if (option (OPTUSEFROMASBCC))
-          msg->env->bcc = mutt_default_from ();
       }
       killfrom = 0;
     }
